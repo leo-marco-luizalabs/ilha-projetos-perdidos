659ba3fd674db144c66afe546fa4afcc3dc368c7
leonardo.marco <leonardo.marco@luizalabs.com>
2025-08-24 21:38:19 -0300

    add pipeline

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,8 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/ilha-projetos-perdidos/', // Nome do seu repositório
   build: {
-    outDir: 'build'
+    outDir: 'dist' // GitHub Pages espera dist/
   }
 })
