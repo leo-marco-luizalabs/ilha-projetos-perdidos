468ca06eeb59b99ef4f4eb617c1749ebcdb82b67
leonardo.marco <leonardo.marco@luizalabs.com>
2025-09-08 22:40:16 -0300

    alter vite

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/', // Nome do seu repositório
+  base: '/ilha-projetos-perdidos/', // Nome do seu repositório
   build: {
     outDir: 'dist' // GitHub Pages espera dist/
   }
