cebaf8bc65262d98ed5211faa5b29a6a10d53b2d
leonardo.marco <leonardo.marco@luizalabs.com>
2025-09-22 21:56:06 -0300

    add new persons

--- src/utils.js
+++ src/utils.js
@@ -13,26 +13,44 @@ export const availableCharacters = [
       name: 'Mago',
       frontImage: 'assets/characters/persona-2-front.png',
       backImage: 'assets/characters/persona-2-back.png',
-      leftImage: 'assets/characters/one-perfil.png', // Usando perfil como lateral
-      rightImage: 'assets/characters/one-front.png', // Variação para direita
+      leftImage: 'assets/characters/persona-2-left.png', // Usando perfil como lateral
+      rightImage: 'assets/characters/persona-2-right.png', 
       color: 'white'
     },
     { 
-    id: 'belo', 
-    name: 'Belo',
-    frontImage: 'assets/characters/persona-3-front.png',
-    backImage: 'assets/characters/persona-3-back.png',
-    leftImage: 'assets/characters/one-perfil.png', // Usando perfil como lateral
-    rightImage: 'assets/characters/one-front.png', // Variação para direita
-    color: 'red'
+      id: 'belo', 
+      name: 'Belo',
+      frontImage: 'assets/characters/persona-3-front.png',
+      backImage: 'assets/characters/persona-3-back.png',
+      leftImage: 'assets/characters/persona-3-left.png', // Usando perfil como lateral
+      rightImage: 'assets/characters/persona-3-right.png', 
+      color: 'red'
   },
   { 
-    id: 'show', 
-    name: 'Show',
-    frontImage: 'assets/characters/persona-4-front.png',
-    backImage: 'assets/characters/persona-4-back.png',
-    leftImage: 'assets/characters/one-perfil.png', // Usando perfil como lateral
-    rightImage: 'assets/characters/one-front.png', // Variação para direita
-    color: 'blue'
+      id: 'show', 
+      name: 'Show',
+      frontImage: 'assets/characters/persona-4-front.png',
+      backImage: 'assets/characters/persona-4-back.png',
+      leftImage: 'assets/characters/persona-4-left.png', // Usando perfil como lateral
+      rightImage: 'assets/characters/persona-4-right.png', 
+      color: 'blue'
+  },
+  { 
+      id: 'show-2', 
+      name: 'Show-2',
+      frontImage: 'assets/characters/persona-6-front.png',
+      backImage: 'assets/characters/persona-6-back.png',
+      leftImage: 'assets/characters/persona-6-left.png', // Usando perfil como lateral
+      rightImage: 'assets/characters/persona-6-right.png', 
+      color: 'grey'
+  },
+  { 
+      id: 'show-3', 
+      name: 'Show-3 ',
+      frontImage: 'assets/characters/persona-6-front.png',
+      backImage: 'assets/characters/persona-6-back.png',
+      leftImage: 'assets/characters/persona-6-left.png', // Usando perfil como lateral
+      rightImage: 'assets/characters/persona-6-right.png', 
+      color: 'yellow'
   }
 ]
